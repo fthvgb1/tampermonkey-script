20e3edfbfc31affc4b12fb0ad3f8f68a9568b818
xing <fthvgb1@163.com>
2020-10-10 10:48:35 +0800

    fix

--- tieba page.js
+++ tieba page.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tieba page
 // @namespace    http://tampermonkey.net/
-// @version      1.013
+// @version      1.014
 // @author       fthvgb1
 // @match        https://tieba.baidu.com/*
 // @match        https://tiebac.baidu.com/*
